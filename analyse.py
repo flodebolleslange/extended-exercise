b366e7fad9a2809ae68bc0145a173c960e342ef8
halfpint16 <Kambeltkoo@duck.com>
2024-11-02 15:39:16 +0000

    hello

--- analyse.py
+++ analyse.py
@@ -4,12 +4,10 @@ import numpy as np
 
 root = r".\lab_data"
 
-
 def analyse_documents(generic_file):
     for file_name in os.listdir(root):
         if file_name[:len(generic_file)] == generic_file:
             data = np.genfromtxt(root + "\\" + file_name, delimiter=',', skip_header=2)
-
 
 # low amplitude
 generic_file = r"test_"
