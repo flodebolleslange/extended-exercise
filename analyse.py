9adf64060131fce1155009c841264775d4a8daef
halfpint16 <Kambeltkoo@duck.com>
2024-11-02 15:42:13 +0000

    hello2

--- analyse.py
+++ analyse.py
@@ -9,6 +9,9 @@ def analyse_documents(generic_file):
         if file_name[:len(generic_file)] == generic_file:
             data = np.genfromtxt(root + "\\" + file_name, delimiter=',', skip_header=2)
 
+            frequency_list = 
+
+
 # low amplitude
 generic_file = r"test_"
 
